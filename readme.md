5cc18b0278cc0279c2c6f4fed1e534521aceede8
azavgo <88772070+azavgo@users.noreply.github.com>
2022-03-03 21:52:11 +1100

    Initial commit supports AUD to USD and XRP to AUD exchange rates

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Utility to get a market snapshot of the selected currencies, cryptos, indexes, and shares.  
+
+### Features: 
+1. Market data is fetched using Alpha Vantage service at https://www.alphavantage.co;
+1. Currently supported: AU$ ⇨ US$ exchange, XRP ⇨ AU$ exchange.
---
+++ script.js
@@ -0,0 +1,49 @@
+const onLoad = async () => { 
+    document.getElementById("audusdOutput").innerHTML = 
+            (await audUSD()).toFixed(2);
+    document.getElementById("xrpaudOutput").innerHTML = 
+            (await xrpAUD()).toFixed(2); 
+}
+
+/*************************************/
+const audUSD = async () => {
+    try {
+        const res = await fetch(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=AUD&to_currency=USD&apikey=TBCROCCJYYPGBQIX`); 
+        const data = await res.json(); 
+        const rate = parseFloat(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]); 
+        return rate; 
+
+    } catch (error) {
+        document.getElementById("audusdOutput").innerHTML = "Unavailable"; 
+    }
+}
+
+const exchangeAUDUSD = async () => { 
+    document.getElementById("audusdOutput").innerHTML = 
+            ((await audUSD()) * document.getElementById("audusdInput").value).toFixed(2); 
+}
+
+/*************************************/
+//TODO: Add AU$ to XRP exchange field. Alpha Vantage only supports XRP to AU$ so I shall need to simply reverse the existing function
+const xrpAUD = async () => {
+    try {
+        const res = await fetch(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=XRP&to_currency=AUD&apikey=TBCROCCJYYPGBQIX`); 
+        const data = await res.json(); 
+        const rate = parseFloat(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]); 
+        return rate; 
+
+    } catch (error) {
+        document.getElementById("xrpaudOutput").innerHTML = "Unavailable"; 
+    }
+}
+
+const exchangeXRPAUD = async () => { 
+    document.getElementById("xrpaudOutput").innerHTML = 
+            ((await xrpAUD()) * document.getElementById("xrpaudInput").value).toFixed(2); 
+}
+/*************************************/
+
+
+
+
+
